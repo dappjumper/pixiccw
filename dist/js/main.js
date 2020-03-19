6a7d9f12dc1479965cd6f166c9c098aab9d83868
Tobias Andersen <dappjump@gmail.com>
2020-03-19 13:44:23 +0100

    Resize to contain inside canvas then trim to the new size

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,47 @@
 class Card {
-	constructor() {
+	constructor(app, canvas) {
 		if(this.visual) this.visual()
+		this.app = app;
+		this.canvas = canvas
 	}
-	addToScene(app) {
-		app.stage.addChild(this.container)
+	addToScene(DOMElement) {
+		let ready = true
+		if(this.container) {
+			if(this.container.width < 2) {
+				ready = false
+			}
+		} else {
+			ready = false
+		}
+		if(!ready) return setTimeout(function(){this.addToScene()}.bind(this), 100)
+		this.container.alpha = 0;
+		this.app.stage.addChild(this.container)
+		console.log(this.canvas)
+		this.containThenTrim()
+		this.fadeIn()
+	}
+	containThenTrim() {
+		//Get height and width ratio between container and app canvas
+		let wRatio = parseFloat(this.app.screen.width) / this.container.width
+		let hRatio = parseFloat(this.app.screen.height) / this.container.height
+
+		//Use the smallest ratio as the global ratio
+		let scaleFactor = (wRatio > hRatio ? hRatio : wRatio)
+
+		//Set the container scale based on scaleFactor
+		this.container.scale.set(scaleFactor, scaleFactor)
+
+		//Resize the canvas to fit container
+		this.canvas.setAttribute('width',(this.container.width)+'px')
+		this.canvas.setAttribute('height',(this.container.height)+'px')
+	}
+	fadeIn() {
+		if(this.container.alpha < 1) {
+			this.container.alpha += 0.1;
+		} else {
+			return this.container.alpha = 1;
+		}
+		requestAnimationFrame(function(){this.fadeIn()}.bind(this))
 	}
 }
 class Mastercard extends Card {
@@ -50,7 +88,7 @@ class Mastercard extends Card {
 
 	DOMElement.appendChild(app.view);
 
-	let demoMastercard = new Mastercard()
-	demoMastercard.addToScene(app)
+	let demoMastercard = new Mastercard(app, DOMElement.querySelector('canvas'))
+	demoMastercard.addToScene()
 
 })();
